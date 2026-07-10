ee479c8f89fc5a79d433bd4a34e55ff6a1b0c994
b4it4ever-collab <b4it4ever@gmail.com>
2026-07-11 01:46:14 +0300

    Add login and enrollment routes
    Added login and enrollment routes with authentication middleware.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 
 // Existing routes...
 $router->get('/', 'HomeController@index');
-
+// Add these to routes/web.php
+$router->get('/login', 'AuthController@showLogin');
+$router->post('/login', 'AuthController@login');
 // New Enrollment Route
 // The 'middleware' => 'AuthMiddleware' will trigger the check before the controller runs
 $router->post('/enroll/{id}', 'EnrollController@enroll', ['middleware' => 'AuthMiddleware']);
